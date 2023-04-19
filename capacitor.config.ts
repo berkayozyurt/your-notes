e58584ca992e41d945f25c1c767a188652aacff7
berkayozyurt <121583372+berkayozyurt@users.noreply.github.com>
2023-04-19 14:39:07 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Your Notes',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
